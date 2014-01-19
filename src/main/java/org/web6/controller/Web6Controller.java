e1e5c0c66ade8299eb3e2726fc0a0645ac2358c9
romanm <roman.mishchenko@gmail.com>
2014-01-19 23:29:59 +0100

    first work commit

---
+++ src/main/java/org/web6/controller/Web6Controller.java
@@ -0,0 +1,55 @@
+package org.web6.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class Web6Controller {
+
+	@RequestMapping("/greeting")
+	public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "greeting";
+	}
+	@RequestMapping("/mysite1")
+	public String mysite1(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+		return "mysite1";
+	}
+	@RequestMapping("/")
+	public String home(Model model) {
+		return "home";
+	}
+	@RequestMapping("/angularjs-The-Basics")
+	public String angularjsTheBasics() {
+		return "angularjs-The-Basics";
+	}
+//	@RequestMapping("/ajs-ajax-1")
+//	public String ajs_ajax_1() {
+//		return "ajs-ajax-1";
+//	}
+
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@RequestMapping("/json")
+	public @ResponseBody Greeting json(
+			@RequestParam(value="name", required=false, defaultValue="World") String name) {
+		return new Greeting(counter.incrementAndGet(),
+				String.format(template, name));
+	}
+//	public @ResponseBody Greeting json2(
+	@RequestMapping( value = "/json2", method = RequestMethod.GET , consumes = {"text/javascript"})
+	public @ResponseBody String json2(
+			@RequestParam(value="name", required=false, defaultValue="World") String name) {
+//		return new Greeting(counter.incrementAndGet(),
+//				String.format(template, name));
+		return "{'id':1,'content':'Hello, World!'}";
+	}
+	
+}
